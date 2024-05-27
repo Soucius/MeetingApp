948ae2af66d10ccef61dc8316f2ceff01b284c30
Soucius <soucius.shopping@gmal.com>
2024-05-27 18:10:33 +0300

    Added libman for manage libraries and used ViewBag and ViewData for dynamic titles.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,14 @@ using Microsoft.AspNetCore.Mvc;
 namespace MeetingApp.Controllers {
     public class HomeController: Controller {
         public IActionResult Index() {
+            int saat = DateTime.Now.Hour;
+
+            // ViewBag.Selamlama = saat > 12 ? "Iyi Gunler" : "Gunaydin";
+            // ViewBag.Username = "Soucius";
+
+            ViewData["Selamlama"] = saat > 12 ? "Iyi Gunler" : "Gunaydin";
+            ViewData["Username"] = "Soucius";
+
             return View();
         }
     }
